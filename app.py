a39ea027d24d775195e129f391ed8ef863029030
shashankshukla48 <149761619+shashankshukla48@users.noreply.github.com>
2025-11-13 00:30:01 +0530

    app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def predict():
     #---------return json format-0-------------------
     return jsonify({'sentiment': sentiment.title()}), 200
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
